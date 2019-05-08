8e2ba0e46dc4a2d11daf1801ad405e860bea973e
Matias Senger <m.senger@hotmail.com>
2019-05-07 23:40:50 -0300

    Improved saving of animation file

--- python/make_animation.py
+++ python/make_animation.py
@@ -14,8 +14,9 @@ SIMULATIONS = [
 	'2019_05_07_18_59_35',
 ]
 
-SIMULATION_TO_ANIMATE = 1
+SIMULATION_TO_ANIMATE = 3
 
+print('Reading data...')
 with open('../simulation_results/' + SIMULATIONS[SIMULATION_TO_ANIMATE] + '/simulation_output.txt') as csvfile:
 	readCSV = csv.reader(csvfile, delimiter='\t')
 	for row in readCSV:
@@ -25,7 +26,8 @@ with open('../simulation_results/' + SIMULATIONS[SIMULATION_TO_ANIMATE] + '/simu
 			crystal_now.append([float(i) for i in row[1+3*k:1+3*k+3]])
 		crystal.append(crystal_now)
 		particle.append([float(i) for i in row[-3:]])
-
+print('Data has been loaded')
+print('Plotting...')
 particles_x = [p[0] for p in crystal[0]]
 particles_x.append(particle[0][0])
 particles_y = [p[1] for p in crystal[0]]
@@ -35,11 +37,12 @@ scat = ax.scatter(
 	x = particles_x,
 	y = particles_y,
 	c = [(.3,.3,.3)]*len(crystal[0]) + [(.7,0,0)],
-	s = 5
+	s = 2
 )
 ax.axis('equal')
 ax.axis('off')
 fig.patch.set_facecolor('black')
+print('Plotting finished')
 
 def cutinone(number):
 	return number if number<1 else 1
@@ -52,10 +55,14 @@ def update(frame_number):
 	scat.set_offsets([[particles_x[i], particles_y[i]] for i in range(len(particles_x))])
 	distancia_en_crystal = [((particles_x[i]-crystal[0][i][0])**2 + (particles_y[i]-crystal[0][i][1])**2)**.3 for i in range(len(crystal[0]))]
 	scat.set_color([(cutinone(.3+2*distancia_en_crystal[i]),cutinone(.3+2*distancia_en_crystal[i]),.3) for i in range(len(crystal[0]))] + [(.7,0,0)])
-	
 
+print('Creating animation...')
 animation = FuncAnimation(fig, update, interval=1, save_count=len(time))
-Writer = mpl_animation.writers['ffmpeg']
-writer = Writer(fps=30, metadata=dict(artist='Me'), bitrate=1800)
-animation.save(SIMULATIONS[SIMULATION_TO_ANIMATE] + '.mp4', writer=writer, savefig_kwargs={'facecolor':'black'})
+
+print('Saving video file...')
+animation.save(
+	SIMULATIONS[SIMULATION_TO_ANIMATE] + '.mp4', 
+	fps = 30,
+	savefig_kwargs = {'facecolor':'black'},
+)
 # ~ plt.show()
