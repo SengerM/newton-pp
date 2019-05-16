abf329ae5c24d76e6e095ff6ee8d0d5e74e9393b
Matias Senger <m.senger@hotmail.com>
2019-05-16 10:09:55 -0300

    Added new argument

--- tools/plot_snapshots.py
+++ tools/plot_snapshots.py
@@ -5,12 +5,13 @@ import matplotlib.pyplot as plt
 
 parser = ArgumentParser()
 parser.add_argument('isimpath', help='directory containing the output data of newton++')
+parser.add_argument('-n', '--nsnapshots', help='number of snapshots to plot', type=int, default=4)
 
 args = parser.parse_args()
 
 system = psys.crystal_and_particle(args.isimpath + '/simulation_output.txt')
 print('Plotting...')
-figs, frame_numbers = psys.plot_snapshots(system)
+figs, frame_numbers = psys.plot_snapshots(system, n_snapshots=args.nsnapshots)
 print('Saving images...')
 if 'snapshots' not in os.listdir(args.isimpath):
 	os.mkdir(args.isimpath + '/snapshots')
