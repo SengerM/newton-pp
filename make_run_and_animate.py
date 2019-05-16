7e612a651dd28cc2449cb8732517c212f4248474
Matias Senger <m.senger@hotmail.com>
2019-05-16 08:04:11 -0300

    First working prototype of this

--- make_run_and_animate.py
+++ make_run_and_animate.py
@@ -1,5 +1,21 @@
 import os
+import tools.psystems as psys
+import matplotlib.pyplot as plt
+
+if 'build' not in os.listdir():
+	os.mkdir('build')
+if 'simulation_results' not in os.listdir():
+	os.mkdir('simulation_results')
 
 os.system('make compile')
 os.system('./newton++')
-os.system('python3 tools/video_from_data_particle_vs_crystal.py')
+
+simulation_number = os.listdir('simulation_results')[-1]
+
+print('Reading data...')
+system = psys.crystal_and_particle('simulation_results/' + simulation_number + '/simulation_output.txt')
+print('Data has been loaded')
+print('Creating animation...')
+animation = psys.animate_system(system)
+
+plt.show()
