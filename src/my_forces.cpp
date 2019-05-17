b9b65e532c34ad024b96d82e11db5aad4a7b02eb
Matias Senger <m.senger@hotmail.com>
2019-05-17 17:43:18 -0300

    Fixed sign mistake

--- src/my_forces.cpp
+++ src/my_forces.cpp
@@ -18,7 +18,7 @@ Vec3D bonding0(Particle &a, Particle &b, std::vector<void*> & params) {
 	if (distance > r1)
 		return Vec3D(0,0,0);
 	if (distance > r0)
-		return vec*(-El/(r1-r0)/vec.Abs());
+		return vec*(El/(r1-r0)/vec.Abs());
 	if (distance > 0)
 		return vec*(-(Eh-El)/r0/vec.Abs());
 	return Vec3D(0,0,0);
