d3e3fee77f6eb02d88b84170c8c83e25ab602119
Matias Senger <m.senger@hotmail.com>
2019-05-22 13:03:26 -0300

    Compatibility with new argument in newton++

--- make_run_and_animate.py
+++ make_run_and_animate.py
@@ -1,15 +1,39 @@
 import os
 import matplotlib.pyplot as plt
+import datetime
+from time import sleep
+
+def get_timestamp():
+	"""
+	Returns a numeric string with a timestamp. It also halts the execution 
+	of the program during 10 micro seconds to ensure that all returned
+	timestamps to be different and unique.
+	
+	Returns
+	-------
+	str
+		String containing the timestamp. Format isYYYYMMDDHHMMSSmmmmmm.
+	
+	Example
+	-------	
+	>>> get_timestamp()
+	'20181013234913378084'
+	>>> [get_timestamp(), get_timestamp()]
+	['20181013235501158401', '20181013235501158583']
+	"""
+	timestamp = datetime.datetime.now().strftime('%Y%m%d%H%M%S%f')
+	sleep(10e-6) # This ensures that there will not exist two equal timestamps.
+	return timestamp
 
 if 'build' not in os.listdir():
 	os.mkdir('build')
 if 'simulation_results' not in os.listdir():
 	os.mkdir('simulation_results')
 
+simulation_timestamp = get_timestamp()
+
 os.system('make clean')
 os.system('make compile')
-os.system('./newton++')
+os.system('./newton++ ' + simulation_timestamp)
 
-sims = os.listdir('simulation_results')
-sims.sort()
-os.system('python3 tools/generate_animation_file.py ' + 'simulation_results/' + sims[-1])
+os.system('python3 tools/generate_animation_file.py ' + 'simulation_results/' + simulation_timestamp)
